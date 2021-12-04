9d3170b4298f363423d31d9929846f276e2e193a
= <ryanfonseca183@gmail.com>
2021-12-04 10:53:38 -0300

    Corrigindo layout das páginas de autenticação

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        @hasSection('title')
+            <title>@yield('title') - {{ config('app.name') }}</title>
+        @else
+            <title>{{ config('app.name') }}</title>
+        @endif
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <script src="https://kit.fontawesome.com/64727a0967.js" crossorigin="anonymous"></script>
+        
+        {{-- BOOTSTRAP 5 --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+        
+        {{-- JQUERY 3 --}}
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="{{  asset('plugins/toastr/toastr.min.css')}}">
+        <script src="{{ asset('plugins/toastr/toastr.min.js') }}"></script>
+        <link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet">
+        <link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css" rel="stylesheet"/>
+ 
+        <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+
+        @stack('assets-head')
+
+        @livewireStyles
+    </head>
+    <body class="font-sans antialiased">
+        @yield('body')
+
+        @livewireScripts
+        <script src="{{ asset('js/custom.js') }}"></script>
+
+        @stack('assets-body')
+    </body>
+</html>
