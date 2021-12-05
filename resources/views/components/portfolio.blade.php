780bcb6bc0cf3c968e9997cc335771fee07672d2
= <ryanfonseca183@gmail.com>
2021-12-05 12:12:48 -0300

    Componente portfolio

---
+++ resources/views/components/portfolio.blade.php
@@ -0,0 +1,12 @@
+@props(['portfolio'])
+
+<a href="#">
+    <div class="card card-portfolio">
+        <img src="{{ asset('storage/' . $portfolio->capa) }}" class="card-img-top card-img-overflow">
+        <div class="card-body">
+            <h6 class="card-subtitle mb-1 text-muted">{{$portfolio->user->nome_artistico}}</h6>
+            <h5 class="card-title card-title-overflow">{{$portfolio->nome}}</h5>
+            
+        </div>
+    </div>
+</a>
